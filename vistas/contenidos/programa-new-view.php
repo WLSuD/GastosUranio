55d450c8445b0115e2e1324bf868c59814624b59
WLSuD <wleensd.2017@gmail.com>
2024-04-10 11:22:23 -0500

    modificacion retorno a dashboard

--- vistas/contenidos/programa-new-view.php
+++ vistas/contenidos/programa-new-view.php
@@ -10,15 +10,8 @@
     <h3 class="text-left "><!-- text-uppercase-->
         <i class="fas fa-calendar-plus fa-fw"></i> &nbsp; Nuevo programa de obra <?php echo $campos['abreviatura'] ?>
     </h3>
-    <!--<?php include "./vistas/desc/desc_programa.php"; ?>-->
-    <div class="row">
-        <div class="col-12 col-md-6">
-            <p class="text-right"><a href="<?php echo SERVERURL; ?>dashboard/" class="btn  btn-dark"><i class="fab fa-dashcube"></i> &nbsp; Regresar a dasboard</a></p>
-        </div>
-        <div class="col-12 col-md-6">
-            <p class="text-right"><a href="<?php echo SERVERURL; ?>programa-menu/<?php echo $lc->encryption($campos['obraId'])?>/" class="btn  btn-dark "><i class="fa fa-diagram-project fa-fw"></i> &nbsp; Regresar a procesos</a></p>
-        </div>
-    </div>
+    <?php include "./vistas/desc/desc_regresos.php"; ?>
+
 </div>
 
 <div class="container-fluid">
---
+++ vistas/desc/desc_regresos.php
@@ -0,0 +1,8 @@
+<div class="row">
+    <div class="col-12 col-md-6">
+        <p class="text-right"><a href="<?php echo SERVERURL; ?>dashboard/" class="btn  btn-dark"><i class="fab fa-dashcube"></i> &nbsp; Regresar a dasboard</a></p>
+    </div>
+    <div class="col-12 col-md-6">
+        <p class="text-right"><a href="<?php echo SERVERURL; ?>programa-menu/<?php echo $lc->encryption($campos['obraId'])?>/" class="btn  btn-dark "><i class="fa fa-diagram-project fa-fw"></i> &nbsp; Regresar a procesos</a></p>
+    </div>
+</div>
